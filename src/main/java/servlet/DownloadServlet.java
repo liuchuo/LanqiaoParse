a7d6f9bed5259ce8447eaf775bcb86f7696ab4b7
JoyHwong <hzy961165750@qq.com>
2016-09-21 14:11:50 +0800

    todo fill the function

---
+++ src/main/java/servlet/DownloadServlet.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DownloadServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7591476022721449364L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+}
