bab8b82215afe9f99408951e81591f241163a7c7
JoyHwong <hzy961165750@qq.com>
2016-09-22 12:23:24 +0800

    reslove conflict

--- src/main/java/servlet/GetSchoolListServlet.java
+++ src/main/java/servlet/GetSchoolListServlet.java
@@ -10,14 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-<<<<<<< HEAD
-@WebServlet(name = "schoolList-servlet", urlPatterns = {"/getSchoolList"})
-=======
 import com.joyhwong.AllSchool;
 
->>>>>>> 9050883477f650e332c73670e130d85343b21726
+@WebServlet(name = "schoolList-servlet", urlPatterns = { "/getSchoolList" })
 public class GetSchoolListServlet extends HttpServlet {
-    private static final long serialVersionUID = -3565728007994021470L;
+	private static final long serialVersionUID = -3565728007994021470L;
 
 	/*
 	 * (non-Javadoc)
@@ -27,25 +24,14 @@ public class GetSchoolListServlet extends HttpServlet {
 	 * 获取文件中的所有学校的名字，存储在一个变量名为schools的SortedSet<String>中
 	 */
 
-<<<<<<< HEAD
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // TODO
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        HttpSession session = request.getSession();
-        SortedSet<String> schools = new TreeSet<String>();
-        schools.add("NanJing");
-        schools.add("HangZhou");
-        session.setAttribute("schools", schools);
-        response.sendRedirect("index.jsp");
-    }
-=======
 		HttpSession session = request.getSession();
 		SortedSet<String> schools = new AllSchool().getAllSchools();
-		
+
 		session.setAttribute("schools", schools);
 		response.sendRedirect("index.jsp");
 	}
->>>>>>> 9050883477f650e332c73670e130d85343b21726
 }
