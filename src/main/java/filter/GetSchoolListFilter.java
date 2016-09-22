1ebdeabb7208290b7864b9a32ff0c4994b3037ac
JoyHwong <hzy961165750@qq.com>
2016-09-22 12:15:59 +0800

    wating to fill

--- src/main/java/filter/GetSchoolListFilter.java
+++ src/main/java/filter/GetSchoolListFilter.java
@@ -8,23 +8,25 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
+@WebFilter(filterName = "schoolList-filter", urlPatterns = {"/index.jsp"})
 public class GetSchoolListFilter implements Filter{
 
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
 
-	// 如果session里面没有schools，则跳转到/getSchoolList，否则执行过滤链
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
-			throws IOException, ServletException {
-		
-		
-	}
+    // 如果session里面没有schools，则跳转到/getSchoolList，否则执行过滤链
+    public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+            throws IOException, ServletException {
 
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
+
+    }
+
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub
+
+    }
 
 }
--- src/main/java/servlet/GetSchoolListServlet.java
+++ src/main/java/servlet/GetSchoolListServlet.java
@@ -5,31 +5,34 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet(name = "schoolList-servlet", urlPatterns = {"/getSchoolList"})
 public class GetSchoolListServlet extends HttpServlet {
-	private static final long serialVersionUID = -3565728007994021470L;
+    private static final long serialVersionUID = -3565728007994021470L;
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.
 	 * HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 * 获取文件中的所有学校的名字，存储在一个变量名为schools的SortedSet<String>中
 	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO
 
-		HttpSession session = request.getSession();
-		SortedSet<String> schools = new TreeSet<String>();
-		schools.add("NanJing");
-		schools.add("HangZhou");
-		session.setAttribute("schools", schools);
-		response.sendRedirect("index.jsp");
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO
+
+        HttpSession session = request.getSession();
+        SortedSet<String> schools = new TreeSet<String>();
+        schools.add("NanJing");
+        schools.add("HangZhou");
+        session.setAttribute("schools", schools);
+        response.sendRedirect("index.jsp");
+    }
 }
