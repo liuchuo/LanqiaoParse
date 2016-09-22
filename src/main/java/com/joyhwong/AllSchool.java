04bc62cade82e56e7bf66a868b6ef5767af4b7dd
JoyHwong <hzy961165750@qq.com>
2016-09-22 16:14:01 +0800

    useless file

--- src/main/java/com/joyhwong/AllSchool.java
+++ src/main/java/com/joyhwong/AllSchool.java
@@ -13,39 +13,18 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class AllSchool {
 
 	private SortedSet<String> schools = new TreeSet<String>();
-	
-	private  File excelFile = new File("2016年软件类-江苏赛区获奖名单.xlsx");
-	
+
+	private File excelFile;
+
+	public AllSchool() {
+		excelFile = new File("/WEB-INF", "2016年软件类-江苏赛区获奖名单.xlsx");
+	}
+
 	public SortedSet<String> getAllSchools() {
-		
-		try {
-            
-            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream(excelFile));           
-            XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);           
-            
-            Iterator<Row> rows = xssfSheet.rowIterator();      
-            rows.next();rows.next();rows.next();
-            
-            while (rows.hasNext()) {
-                
-                XSSFRow row = (XSSFRow) rows.next();
-                XSSFCell cell = row.getCell(1);
-                
-                if (!schools.contains(cell.getStringCellValue())) {
-                	schools.add(cell.getStringCellValue());
-                }
-                
-            }
-            xssfWorkbook.close();
-            
-		}catch (IOException e) {
-            e.printStackTrace();
-        }
-		
-		return schools;
+
+			return schools;
 	}
 }
